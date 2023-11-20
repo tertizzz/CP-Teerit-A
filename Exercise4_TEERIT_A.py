8427fd0d504069b0235aa4f87df3ac8b04690cb6
tertizzz <tertizzz@users.noreply.github.com>
2023-11-21 01:42:53 +0700

    Update Exercise4_TEERIT_A.py

--- Exercise4_TEERIT_A.py
+++ Exercise4_TEERIT_A.py
@@ -2,4 +2,8 @@ FE=92.5 #Foundation English
 GB=93 #General Business 
 ICS=97.8 #Introduction to Computer Systems 
 Compro=89.9 #Computer Programming 
-Print"--- Your Score ---"
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", Compro)
